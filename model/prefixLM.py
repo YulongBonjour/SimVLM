470071f3dd1c494e7890b34377a3e0bf797cd5ab
Yulong <67751093+yulong-XJTU@users.noreply.github.com>
2022-09-26 10:08:06 +0200

    Update prefixLM.py

--- model/prefixLM.py
+++ model/prefixLM.py
@@ -1,92 +1,91 @@
+'''
+   author: yulong-XJTU
+'''
 import torch
 from torch import nn
 import torch.nn.functional as F
 import copy
 from model.transformer import Transformer,subsequent_mask
+from axial_positional_embedding import AxialPositionalEmbedding
+from model.resblock import BottleneckBlock
 from random import randint
 from einops import rearrange
 def clone(module,N):
     '''copy the given module N times'''
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-class ResBlock(nn.Module):
-    def __init__(self, chan):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(chan, chan, 3, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(chan, chan, 3, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(chan, chan, 1)
-        )
 
-    def forward(self, x):
-        return self.net(x) + x
 class PrefixLM(nn.Module):
     def __init__(
             self,
             d_model=512,
             input_resolution=224,
             patch_size=16,
-            num_text_tokens=20000,
+            num_text_tokens=10000,
             txt_seq_len=256,
+            prefix_txt_len=25,
+            target_txt_len=52,
+            max_trunc_txt_len=15,
             heads=8,
-            enc_depth=8,
-            dec_depth=8,
-            res_depth=3,
+            enc_depth=12,
+            dec_depth=12,
             d_ff=1024,
-            dropout=0.1
+            dropout=0.
     ):
         super(PrefixLM,self).__init__()
-        assert input_resolution%patch_size==0
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=d_model, kernel_size=patch_size, stride=patch_size, bias=False)
-        self.resnet=nn.Sequential(*[nn.Sequential(ResBlock(d_model)) for _ in range(res_depth)])        #nn.Sequential(clone(ResBlock(d_model),res_depth))
-        self.txt_embed=nn.Embedding(num_text_tokens+2,d_model)#<seg>和<eof>
-        self.txt_pos_embed=nn.Embedding(txt_seq_len*2,d_model)#encoder和decoder中txt长度均为256
-        self.img_tokens_len=(input_resolution // patch_size) ** 2
-        self.img_pos_embed=nn.Embedding(self.img_tokens_len,d_model)
+        assert input_resolution%patch_size==0 and max_trunc_txt_len<=prefix_txt_len and max_trunc_txt_len<txt_seq_len
+        self.ResNet=nn.Sequential(*[nn.Conv2d(in_channels=3, out_channels=64, kernel_size=patch_size, stride=patch_size, bias=True),
+                                    BottleneckBlock(in_channels=64,out_channels=256,bottleneck_channels=64,),
+                                    BottleneckBlock(in_channels=256,out_channels=d_model,bottleneck_channels=128)])
+        self.txt_embed = nn.Embedding( num_text_tokens, d_model)
+        self.txt_pos_embed=nn.Embedding(prefix_txt_len+target_txt_len,d_model)
+        image_fmap_size=input_resolution // patch_size
+        self.img_tokens_len=image_fmap_size ** 2
+        # self.img_pos_embed=nn.Embedding(self.img_tokens_len,d_model)
+        self.img_pos_embed =  AxialPositionalEmbedding(d_model, axial_shape = (image_fmap_size, image_fmap_size))
         self.txt_seq_len=txt_seq_len
+        self.target_txt_len=target_txt_len
+        self.prefix_txt_len = prefix_txt_len
+        self.max_trunc_txt_len=max_trunc_txt_len
         self.num_text_tokens=num_text_tokens
         self.dim_embed=d_model
         self.input_resolution=input_resolution
         self.patch_size=patch_size
-        #self.register_buffer('tgt_mask', tgt_mask, persistent=False)
+        # self.temperature = nn.Parameter(torch.tensor(1.))#论文中没提到
         self.transformer=Transformer(d_model,heads,enc_depth,dec_depth,d_ff,dropout=dropout)
         self.to_logits = nn.Sequential(
             nn.LayerNorm(d_model),
-            nn.Linear(d_model, self.num_text_tokens+2),
+            nn.Linear(d_model,  num_text_tokens)
         )
     def forward(self,img,txt,return_loss=False):
         device=txt.device
-        img_emed=self.conv1(img)
-        img_emed=self.resnet(img_emed)
+        n=txt.shape[0]
+        img_emed=self.ResNet(img)
         img_emed=rearrange(img_emed,'b c h w -> b (h w) c')
-        img_emed+=self.img_pos_embed(torch.arange(self.img_tokens_len,device=device))
-        seg=torch.zeros(self.txt_seq_len,device=device,dtype=torch.long)+self.num_text_tokens+1#<seg>
-        end=torch.zeros(self.txt_seq_len,device=device,dtype=torch.long)+self.num_text_tokens+2#<end>
-        l=randint(0,8)
-
-        pre_txt=torch.zeros_like(txt)
+        img_emed=img_emed+self.img_pos_embed(img_emed)
+        l = randint(0,self.max_trunc_txt_len)
+        pre_txt=torch.zeros((n, self.prefix_txt_len),device=device,dtype=torch.long)
         pre_txt[:,:l]=txt[:,:l]
-        tgt_txt=torch.zeros_like(txt)
+        tgt_txt=torch.zeros((n,self.target_txt_len),device=device,dtype=torch.long)
         tgt_txt[:,:(self.txt_seq_len-l)]=txt[:,l:]
         del txt,img
-        pre_txt=torch.where(pre_txt==0,seg,pre_txt)
-        tgt_txt=torch.where(tgt_txt==0,end,tgt_txt)
-        tgt_txt= F.pad(tgt_txt, (1, 0), value = 0)#add<bos>
+
+        tgt_txt= F.pad(tgt_txt, (1, 0), value = 4)#add<CLS>
         labels=tgt_txt[:,1:]
         tgt_txt=tgt_txt[:,:-1]#
-        del seg, end
+
         pre_txt_embed=self.txt_embed(pre_txt)
-        pre_txt_embed+=self.txt_pos_embed(torch.arange(self.txt_seq_len,device=device))
+        pre_txt_embed=pre_txt_embed+self.txt_pos_embed(torch.arange(self.prefix_txt_len,device=device))
         tgt_txt_embed=self.txt_embed(tgt_txt)
-        tgt_txt_embed+=self.txt_pos_embed(torch.arange(self.txt_seq_len,device=device)+self.txt_seq_len)
+        tgt_txt_embed=tgt_txt_embed+self.txt_pos_embed(torch.arange(self.target_txt_len,device=device)+self.prefix_txt_len)
 
         prefix=torch.cat((img_emed,pre_txt_embed),dim=1)
-        tgt_mask=subsequent_mask(self.txt_seq_len).to(device)
+        tgt_mask=subsequent_mask(self.target_txt_len).to(device)
         out=self.transformer(prefix,tgt_txt_embed,tgt_mask=tgt_mask)
         logits=self.to_logits(out)
         if not return_loss:
             return logits
+        # temp = self.temperature.exp()
         logits = rearrange(logits, 'b n c -> b c n')
-        loss=F.cross_entropy(logits,labels)
+        # logits=logits*temp #带温度参数
+        loss=F.cross_entropy(logits,labels,ignore_index=0)
         return loss
